8b319465a9590b9df08b8a33a2f8cd0a6999a0bb
Nikhil Nygaard <nikhil.nygaard@gmail.com>
2018-08-14 16:35:56 -0700

    don't validate issuer

--- app.js
+++ app.js
@@ -47,28 +47,28 @@ var log = bunyan.createLogger({
 //   serialize users into and deserialize users out of the session.  Typically,
 //   this will be as simple as storing the user ID when serializing, and finding
 //   the user by ID when deserializing.
-passport.serializeUser(function(user, done) {
-  done(null, user.email);
+passport.serializeUser(function (user, done) {
+    done(null, user.email);
 });
 
-passport.deserializeUser(function(id, done) {
-  findByEmail(id, function (err, user) {
-    done(err, user);
-  });
+passport.deserializeUser(function (id, done) {
+    findByEmail(id, function (err, user) {
+        done(err, user);
+    });
 });
 
 // array to hold logged in users
 var users = [];
 
-var findByEmail = function(email, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-   log.info('we are using user: ', user);
-    if (user.email === email) {
-      return fn(null, user);
+var findByEmail = function (email, fn) {
+    for (var i = 0, len = users.length; i < len; i++) {
+        var user = users[i];
+        log.info('we are using user: ', user);
+        if (user.email === email) {
+            return fn(null, user);
+        }
     }
-  }
-  return fn(null, null);
+    return fn(null, null);
 };
 
 // Use the OIDCStrategy within Passport. (Section 2) 
@@ -77,35 +77,36 @@ var findByEmail = function(email, fn) {
 //   credentials (in this case, an OpenID identifier), and invoke a callback
 //   with a user object.
 passport.use(new OIDCStrategy({
-    callbackURL: config.creds.returnURL,
-    realm: config.creds.realm,
-    clientID: config.creds.clientID,
-    clientSecret: config.creds.clientSecret,
-    oidcIssuer: config.creds.issuer,
-    identityMetadata: config.creds.identityMetadata,
-    skipUserProfile: config.creds.skipUserProfile,
-    responseType: config.creds.responseType,
-    responseMode: config.creds.responseMode
-  },
-  function(iss, sub, profile, accessToken, refreshToken, done) {
-    if (!profile.email) {
-      return done(new Error("No email found"), null);
+        callbackURL: config.creds.returnURL,
+        realm: config.creds.realm,
+        clientID: config.creds.clientID,
+        clientSecret: config.creds.clientSecret,
+        oidcIssuer: config.creds.issuer,
+        identityMetadata: config.creds.identityMetadata,
+        skipUserProfile: config.creds.skipUserProfile,
+        responseType: config.creds.responseType,
+        responseMode: config.creds.responseMode,
+        validateIssuer: false,
+    },
+    function (iss, sub, profile, accessToken, refreshToken, done) {
+        if (!profile.email) {
+            return done(new Error("No email found"), null);
+        }
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+            findByEmail(profile.email, function (err, user) {
+                if (err) {
+                    return done(err);
+                }
+                if (!user) {
+                    // "Auto-registration"
+                    users.push(profile);
+                    return done(null, profile);
+                }
+                return done(null, user);
+            });
+        });
     }
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      findByEmail(profile.email, function(err, user) {
-        if (err) {
-          return done(err);
-        }
-        if (!user) {
-          // "Auto-registration"
-          users.push(profile);
-          return done(null, profile);
-        }
-        return done(null, user);
-      });
-    });
-  }
 ));
 
 
@@ -114,38 +115,38 @@ passport.use(new OIDCStrategy({
 var app = express();
 
 
-app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.logger());
-  app.use(express.methodOverride());
-  app.use(cookieParser());
-  app.use(expressSession({ secret: 'keyboard cat', resave: true, saveUninitialized: false }));
-  app.use(bodyParser.urlencoded({ extended : true }));
-  // Initialize Passport!  Also use passport.session() middleware, to support
-  // persistent login sessions (recommended).
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/../../public'));
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.logger());
+    app.use(express.methodOverride());
+    app.use(cookieParser());
+    app.use(expressSession({secret: 'keyboard cat', resave: true, saveUninitialized: false}));
+    app.use(bodyParser.urlencoded({extended: true}));
+    // Initialize Passport!  Also use passport.session() middleware, to support
+    // persistent login sessions (recommended).
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/../../public'));
 });
 
 //Routes (Section 4)
 
-app.get('/', function(req, res){
-  res.render('index', { user: req.user });
+app.get('/', function (req, res) {
+    res.render('index', {user: req.user});
 });
 
-app.get('/account', ensureAuthenticated, function(req, res){
-  res.render('account', { user: req.user });
+app.get('/account', ensureAuthenticated, function (req, res) {
+    res.render('account', {user: req.user});
 });
 
 app.get('/login',
-  passport.authenticate('azuread-openidconnect', { failureRedirect: '/login' }),
-  function(req, res) {
-    log.info('Login was called in the Sample');
-    res.redirect('/');
-});
+    passport.authenticate('azuread-openidconnect', {failureRedirect: '/login'}),
+    function (req, res) {
+        log.info('Login was called in the Sample');
+        res.redirect('/');
+    });
 
 // POST /auth/openid
 //   Use passport.authenticate() as route middleware to authenticate the
@@ -154,11 +155,11 @@ app.get('/login',
 //   provider will redirect the user back to this application at
 //   /auth/openid/return
 app.get('/auth/openid',
-  passport.authenticate('azuread-openidconnect', { failureRedirect: '/login' }),
-  function(req, res) {
-    log.info('Authentication was called in the Sample');
-    res.redirect('/');
-  });
+    passport.authenticate('azuread-openidconnect', {failureRedirect: '/login'}),
+    function (req, res) {
+        log.info('Authentication was called in the Sample');
+        res.redirect('/');
+    });
 
 // GET /auth/openid/return
 //   Use passport.authenticate() as route middleware to authenticate the
@@ -166,11 +167,11 @@ app.get('/auth/openid',
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
 app.get('/auth/openid/return',
-  passport.authenticate('azuread-openidconnect', { failureRedirect: '/login' }),
-  function(req, res) {
-    log.info('We received a return from AzureAD.');
-    res.redirect('/');
-  });
+    passport.authenticate('azuread-openidconnect', {failureRedirect: '/login'}),
+    function (req, res) {
+        log.info('We received a return from AzureAD.');
+        res.redirect('/');
+    });
 
 // GET /auth/openid/return
 //   Use passport.authenticate() as route middleware to authenticate the
@@ -178,15 +179,15 @@ app.get('/auth/openid/return',
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
 app.post('/auth/openid/return',
-  passport.authenticate('azuread-openidconnect', { failureRedirect: '/login' }),
-  function(req, res) {
-    log.info('We received a return from AzureAD.');
-    res.redirect('/');
-  });
+    passport.authenticate('azuread-openidconnect', {failureRedirect: '/login'}),
+    function (req, res) {
+        log.info('We received a return from AzureAD.');
+        res.redirect('/');
+    });
 
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
 });
 
 
@@ -200,6 +201,8 @@ app.listen(3000);
 //   the request will proceed.  Otherwise, the user will be redirected to the
 //   login page.
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login')
 }
